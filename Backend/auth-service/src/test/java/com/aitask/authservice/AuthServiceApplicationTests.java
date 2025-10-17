57399b441f382a314a1694790bc1e8bee5a584b5
Chetan Purkar <157473249+Chetan-Purkar@users.noreply.github.com>
2025-10-18 00:44:27 +0530

    Initial commit

---
+++ Backend/auth-service/src/test/java/com/aitask/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aitask.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
